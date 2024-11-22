218c8999391dc6a08a01a0c2c6cd8c3dd5427639
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2024-11-22 23:58:06 +0530

    add login page authentication and form validation

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,174 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useAuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { logIn, googleSignIn } = useAuthContext();
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    try {
+      await logIn(data.email, data.password);
+      navigate("/board");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+      navigate("/board");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <main>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-4 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign in
+          </h2>
+        </div>
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <button
+            aria-label="Continue with google"
+            role="button"
+            className="mt-10 flex w-full items-center justify-center rounded-md border border-gray-300 px-4 py-2 hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-1"
+          >
+            <svg
+              width={19}
+              height={20}
+              viewBox="0 0 19 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M18.9892 10.1871C18.9892 9.36767 18.9246 8.76973 18.7847 8.14966H9.68848V11.848H15.0277C14.9201 12.767 14.3388 14.1512 13.047 15.0812L13.0289 15.205L15.905 17.4969L16.1042 17.5173C17.9342 15.7789 18.9892 13.221 18.9892 10.1871Z"
+                fill="#4285F4"
+              />
+              <path
+                d="M9.68813 19.9314C12.3039 19.9314 14.4999 19.0455 16.1039 17.5174L13.0467 15.0813C12.2286 15.6682 11.1306 16.0779 9.68813 16.0779C7.12612 16.0779 4.95165 14.3395 4.17651 11.9366L4.06289 11.9465L1.07231 14.3273L1.0332 14.4391C2.62638 17.6946 5.89889 19.9314 9.68813 19.9314Z"
+                fill="#34A853"
+              />
+              <path
+                d="M4.17667 11.9366C3.97215 11.3165 3.85378 10.6521 3.85378 9.96562C3.85378 9.27905 3.97215 8.6147 4.16591 7.99463L4.1605 7.86257L1.13246 5.44363L1.03339 5.49211C0.37677 6.84302 0 8.36005 0 9.96562C0 11.5712 0.37677 13.0881 1.03339 14.4391L4.17667 11.9366Z"
+                fill="#FBBC05"
+              />
+              <path
+                d="M9.68807 3.85336C11.5073 3.85336 12.7344 4.66168 13.4342 5.33718L16.1684 2.59107C14.4892 0.985496 12.3039 0 9.68807 0C5.89885 0 2.62637 2.23672 1.0332 5.49214L4.16573 7.99466C4.95162 5.59183 7.12608 3.85336 9.68807 3.85336Z"
+                fill="#EB4335"
+              />
+            </svg>
+            <span
+              onClick={handleGoogleSignIn}
+              className="ml-4 text-base font-medium text-gray-700"
+            >
+              Continue with Google
+            </span>
+          </button>
+          <div className="flex w-full items-center justify-between pt-5">
+            <hr className="w-full bg-gray-400" />
+            <p className="px-2.5 text-base font-medium leading-4 text-gray-400">
+              OR
+            </p>
+            <hr className="w-full bg-gray-400" />
+          </div>
+        </div>
+        <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                {" "}
+                Email{" "}
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Enter email..."
+                  autoComplete="email"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                      message: "Enter a valid email",
+                    },
+                  })}
+                  className="block w-full rounded-md border-0 px-4 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                />
+                {errors.email && (
+                  <span className="text-sm text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  {" "}
+                  Password{" "}
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Enter password..."
+                  autoComplete="current-password"
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                  className="block w-full rounded-md border-0 px-4 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+                />
+                {errors.password && (
+                  <span className="text-sm text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-4 py-2 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+          <p className="mt-10 text-center text-sm/6 text-gray-500">
+            Don't have an account?
+            <span className="cursor-pointer font-semibold text-indigo-600 hover:text-indigo-500">
+              Sign Up
+            </span>
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
