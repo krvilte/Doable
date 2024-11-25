1a161b4de3fb09de1b65aa5ee0348f3689d69c77
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2024-11-25 22:54:24 +0530

    setup redux store

--- src/pages/board/index.jsx
+++ src/pages/board/index.jsx
@@ -9,8 +9,8 @@ const Board = () => {
           <h1 className="text-xl font-semibold">Eisenhower Matrix</h1>
 
           <div className="mx-auto mt-8 grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-            <div className="p-4 ml-auto w-full max-w-[460px] rounded border border-stone-400 shadow-sm">
-              <h2 className="text-lg pb-2 font-semibold border-b-[1px] border-stone-500">
+            <div className="p-4 ml-auto w-full max-w-[460px] rounded border border-black/50 shadow-sm">
+              <h2 className="text-lg pb-2 font-semibold border-b-[1px] border-black/20">
                 DO FIRST
               </h2>
               <ol className="flex flex-col py-2 list-decimal">
@@ -19,7 +19,7 @@ const Board = () => {
                     <input
                       id="default-checkbox"
                       type="checkbox"
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600"
                     />
                     <span>Write a blog post</span>
                   </div>
@@ -29,7 +29,7 @@ const Board = () => {
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       className="w-5 h-5"
                       onClick={() => {
@@ -40,8 +40,8 @@ const Board = () => {
                       }}
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
                       />
                     </svg>
@@ -49,7 +49,7 @@ const Board = () => {
                 </li>
               </ol>
 
-              <button className="text-left text-base text-black/70 rounded bg-transparent border-none px-4 py-1 mt-2 w-full cursor-pointer hover:bg-gray-200 focus:bg-gray-100 ">
+              <button className="text-left text-base w-full px-4 py-1 rounded text-black bg-transparent hover:bg-gray-200">
                 + Add Task
               </button>
             </div>
