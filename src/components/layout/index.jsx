479cdd8f356901e1a5f2ec5de046b99fecdc0709
Vishal Kumar Sah <thevishalkumar25@gmail.com>
2024-11-25 19:08:02 +0530

    add updated matrix board

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -4,10 +4,10 @@ import Sidebar from "../sidebar";
 const Layout = ({ children }) => {
   return (
     <div className="flex bg-white">
-      <aside className="flex w-16 gap-x-4 bg-stone-800 fixed top-0 left-0 z-40 lg:w-60 h-screen">
+      <aside className="flex w-16 gap-x-4 bg-stone-800 fixed top-0 left-0 z-40 lg:w-64 h-screen">
         <Sidebar />
       </aside>
-      <main className="flex-1 bg-gray-50 ml-16 lg:ml-60 h-screen ">
+      <main className="flex-1 bg-gray-50 ml-16 lg:ml-64 h-screen ">
         <Outlet />
         {children}
       </main>
--- src/pages/board/index.jsx
+++ src/pages/board/index.jsx
@@ -8,49 +8,50 @@ const Board = () => {
         <div className="m-4">
           <h1 className="text-xl font-semibold">Eisenhower Matrix</h1>
 
-          <div className=" mx-auto mt-8">
-            <div className="flex">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-                <div className="bg-gray-200 p-4 border border-black">
-                  <div className="text-left font-bold text-xl">DO</div>
-                  <p>Do it now.</p>
-                  <ol className="list-decimal ml-4">
-                    <li>Write a blog post</li>
-                    <li>
-                      Write tests for a functionality and push the changes
-                    </li>
-                    <li>Prepare for tomorrow’s talk</li>
-                    <li>Review pull request</li>
-                  </ol>
-                </div>
-                <div className="bg-gray-200 p-4 border border-black">
-                  <div className="text-left font-bold text-xl">DECIDE</div>
-                  <p>Schedule a time to do it.</p>
-                  <ol className="list-decimal ml-4">
-                    <li>Plan for alumni meet</li>
-                    <li>Read about cloud design patterns</li>
-                    <li>Big refactoring</li>
-                  </ol>
-                </div>
-                <div className="bg-gray-200 p-4 border border-black">
-                  <div className="text-left font-bold text-xl">DELEGATE</div>
-                  <p>Who can do it for you?</p>
-                  <ol className="list-decimal ml-4">
-                    <li>Creating interview process for junior engineers</li>
-                    <li>Reviewing a blog</li>
-                    <li>Taking interview</li>
-                  </ol>
-                </div>
-                <div className="bg-gray-200 p-4 border border-black">
-                  <div className="text-left font-bold text-xl">DELETE</div>
-                  <p>Eliminate it.</p>
-                  <ol className="list-decimal ml-4">
-                    <li>A meeting with no agenda</li>
-                    <li>Checking social media</li>
-                    <li>Long tea/coffee breaks</li>
-                  </ol>
-                </div>
-              </div>
+          <div className="mx-auto mt-8 grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+            <div className="p-4 ml-auto w-full max-w-[460px] rounded border border-stone-400 shadow-sm">
+              <h2 className="text-lg pb-2 font-semibold border-b-[1px] border-stone-500">
+                DO FIRST
+              </h2>
+              <ol className="flex flex-col py-2 list-decimal">
+                <li className="flex justify-between list-none px-4 p-1 rounded cursor-pointer text-black hover:bg-gray-200">
+                  <div className="flex items-center gap-x-2">
+                    <input
+                      id="default-checkbox"
+                      type="checkbox"
+                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <span>Write a blog post</span>
+                  </div>
+
+                  <div className="flex gap-x-2 items-center ">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                      onClick={() => {
+                        <div className="bg-green-500">
+                          <button>Edit</button>
+                          <button>Delete</button>
+                        </div>;
+                      }}
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+                      />
+                    </svg>
+                  </div>
+                </li>
+              </ol>
+
+              <button className="text-left text-base text-black/70 rounded bg-transparent border-none px-4 py-1 mt-2 w-full cursor-pointer hover:bg-gray-200 focus:bg-gray-100 ">
+                + Add Task
+              </button>
             </div>
           </div>
         </div>
